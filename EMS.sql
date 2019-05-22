a8bacb8b828c02094389b7637e602ced977d5be1
edls2013 <edls2013@tamu.edu>
2019-05-22 14:46:40 -0500

    Added entry for Tina Smith into EmpDetails table

--- EMS.sql
+++ EMS.sql
@@ -38,9 +38,13 @@ INSERT INTO EmpDetails(ID, EmployeeID, Salary, Address1, Address2, City, State, 
 VALUES	(1, 401, 80000.00, 'East Eighth Street', 'Apt 6', 'Boulder', 'CO', 'US'),
 		(2, 402, 60000.00, 'Shrute Farms', 'Homestead 1', 'Scranton', 'PA', 'US'),
 		(3, 403, 30000.00, 'Halpert Place', '', 'Scranton', 'PA', 'US');
-		
-INSERT INTO Employee(ID, FirstName, LastName, SSN, DeptID)
-VALUES	(410, 'Tina', 'Smith', '999119999', 803);
 
 INSERT INTO Department(ID, Name, Location)
 VALUES	(810, 'Marketing', 'Philadelphia');
+
+INSERT INTO Employee(ID, FirstName, LastName, SSN, DeptID)
+VALUES	(410, 'Tina', 'Smith', '999119999', 810);
+
+INSERT INTO EmpDetails(ID, EmployeeID, Salary, Address1, Address2, City, State, Country)
+VALUES	(4, 410, 50000.00, 'Fifth Avenue', 'Suite 301', 'New York', 'NY', 'US');
+
